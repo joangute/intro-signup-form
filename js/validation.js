98901b4d915ffc10a6aa03913720579a4198c2ce
Angel <angel_gt45@hotmail.com>
2022-09-27 14:24:41 -0500

    first commit

---
+++ js/validation.js
@@ -0,0 +1,31 @@
+const inputs=document.querySelectorAll("input");
+
+for(let i=0;i<inputs.length;i++)
+{
+
+  inputs[i].addEventListener("focus",function(){
+    if(this.placeholder=="Email Address" && this.classList.contains("error_input"))
+      this.value="";
+    this.classList.remove("error_input");
+    this.nextElementSibling.innerHTML="";
+    this.style.setProperty("--color_placeholder","hsla(249, 10%, 26%,.7)");
+  });
+
+  inputs[i].addEventListener("invalid",function(e){
+      e.preventDefault();
+      this.classList.add("error_input");
+      if(this.value=="")
+      {
+        this.nextElementSibling.innerHTML= this.placeholder+" can not be empty";
+        this.style.setProperty("--color_placeholder","transparent");
+      }
+      else{
+        if(this.placeholder=="Email Address")
+        {
+         this.value="email@example/com";
+         this.nextElementSibling.innerHTML=" Looks like this is not an email";
+        }
+      }  
+  });
+  
+}
